5b8153b4593e33cae52b3ab2ce037aff2d1dd63b
pengprond-ruangrairattanaroj-dragon <pengprond.ruangrairattanaroj@moxbank.com>
2023-02-22 13:34:08 +0800

    (test): wkfs eom

--- compare-two-csv.py
+++ compare-two-csv.py
@@ -14,7 +14,7 @@ for file_location_1 in files1:
     print(f"Here is the file : {file_name_1}")
     if file_location_2 in files2 :
         df1 = pd.read_csv(file_location_1, sep=";", index_col=False)
-        df2 = pd.read_csv(file_location_2, sep=",", index_col=[0])
+        df2 = pd.read_csv(file_location_2, sep=";", index_col=False)
         df_diff = pd.concat([df1,df2]).drop_duplicates(keep=False)
 
         df_diff.to_csv(path3+"/"+file_name_1, sep=";", index=False)
